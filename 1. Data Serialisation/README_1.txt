4ac33cefa74db9b66976e779e76b1696c912925c
sab117 <62747968+sab117@users.noreply.github.com>
2022-03-14 18:38:56 +0000

    File 1

---
+++ 1. Data Serialisation/README_1.txt
@@ -0,0 +1,12 @@
+Last Modified 23/02/2022
+By Saboor Rashid
+
+
+All the data in the folder converts the excel file into a pickle format
+
+"Full Set"--> Contains all the Raw Data i.e results of Acclerated Ageing Test (AAT) 
+"Set 1" --> Contains the first hour of Raw data
+"Set 2" --> Contains the second hour of Raw Data
+"Set 3" --> Contains the third hour of Raw Data
+"Set 4" --> Contians the fourth hour of Raw Data
+
---
+++ 1. Data Serialisation/Set1/Read.py
@@ -0,0 +1,33 @@
+#Last modified 03/02/2022
+#By Saboor Rashid
+#Code converts Excel code to a pickle file
+#Code is actually from this website "https://www.geeksforgeeks.org/how-to-read-all-excel-files-under-a-directory-as-a-pandas-dataframe/"
+
+
+import pandas as pd
+import os
+import glob
+
+  
+  
+# use glob to get all the xlsx files
+# in the folder
+path = os.getcwd()
+csv_files = glob.glob(os.path.join(path, "*.xlsx"))
+  
+  
+# loop over the list of csv files
+for f in csv_files:
+    
+    # read the csv file
+    df = pd.read_excel(f)
+      
+    # print the location and filename
+    print('Location:', f)
+    print('File Name:', f.split("\\")[-1])
+      
+    # print the content
+    print('Content:')
+    #display(df)
+    print(df)
+    df.to_pickle("Set_1") #Change file name depending on Excel File
